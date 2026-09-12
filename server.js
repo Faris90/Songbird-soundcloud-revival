85fc6d44e0a794cec8f5e292ad1b3437b45fcbc9
Faris Soliman <faris.soliman@yahoo.com>
2026-09-11 22:54:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const axios = require('axios');
+import express from 'express';
+import axios from 'axios';
 const { spawn } = require('child_process');
 const path = require('path');
 const fs = require('fs');
